6e88b69b8d7cd88e869a33ded84c8b421ee067f2
Fernanda Fernandes do Nascimento <106761954+Fernandeezz@users.noreply.github.com>
2023-05-02 14:59:07 -0300

    Update e_ou_não_multiplos.php

--- e_ou_não_multiplos.php
+++ e_ou_não_multiplos.php
@@ -1,3 +1,4 @@
+<?php
 //um programa que leia um número e imprima uma das duas mensagens: "É múltiplo de 3"ou "Não é múltiplo de 3".
 
 $numero = intval(readline("Digite um número: "));
@@ -6,3 +7,4 @@ if ($numero % 3 == 0) {
   } else {
         echo "Não é múltiplo de 3\n";
 }
+?>
