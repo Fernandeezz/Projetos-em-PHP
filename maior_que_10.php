842aaed43f07c7aba8012482a0c9767ae27f415c
Fernanda Fernandes do Nascimento <106761954+Fernandeezz@users.noreply.github.com>
2023-05-02 14:59:41 -0300

    Update maior_que_10.php

--- maior_que_10.php
+++ maior_que_10.php
@@ -1,3 +1,4 @@
+<?php
 // um programa PHP que leia dois valores numéricos inteiros e efetue a adição, caso o resultado seja maior que 10, apresentá-lo.
 
 $multiplicando = intval(readline("Digite o multiplicando: "));
@@ -8,3 +9,4 @@ for ($i = 1; $i <= $multiplicador; $i++) {
 }
 echo "O produto é: " . $produto;
 echo '<br>';
+?>
